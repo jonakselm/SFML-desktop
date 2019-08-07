ad77f4061590cec9dc6ad043e4f64f3dae4c49d3
Jon Aksel <jonakselm@gmail.com>
2019-08-07 17:53:55 +0200

    made a ps3 class and gamecube class in sfExtensions, these classes each of these have their own isButtonPressed, use these to only make that joystick Id work for inputs

--- SFML-desktop/SFML-ext.hpp
+++ SFML-desktop/SFML-ext.hpp
@@ -5,10 +5,61 @@ namespace sfExt
 	class Joystick
 	{
 	public:
+	};
+
+	class Ps3
+	{
+	public:
 		// Enum for cycling through the buttons easier
-		enum Button {
+		enum Button
+		{
 			Select, L3, R3, Start, Up, Right, Down, Left, L2, R2,
 			L1, R1, Triangle, Circle, Cross, Square, PS_Btn
 		};
+
+		static unsigned int getProductId()
+		{
+			return 616;
+		}
+
+		static bool isButtonPressed(unsigned int joystick, unsigned int button)
+		{
+			unsigned int id = sf::Joystick::getIdentification(joystick).productId;
+			if (id == getProductId())
+			{
+				if (sf::Joystick::isButtonPressed(joystick, button))
+					return true;
+			}
+			return false;
+		}
+
+	private:
+
+	};
+
+	class GameCube
+	{
+	public:
+		// Enum for cycling through the buttons easier
+		enum Button
+		{
+			X, A, B, Y, L, R, Z = 7, Start = 9, Up = 12, Right, Down, Left
+		};
+
+		static unsigned int getProductId()
+		{
+			return 6214;
+		}
+
+		static bool isButtonPressed(unsigned int joystick, unsigned int button)
+		{
+			unsigned int id = sf::Joystick::getIdentification(joystick).productId;
+			if (id == getProductId())
+			{
+				if (sf::Joystick::isButtonPressed(joystick, button))
+					return true;
+			}
+			return false;
+		}
 	};
 }
